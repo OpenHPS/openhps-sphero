e23808635a1054e2bea6f1b0b8a87d61fbe4a77b
Maxim Van de Wynckel <maxim.van.de.wynckel@vub.be>
2022-05-11 15:39:39 +0200

    chore: github workflow

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,22 +6,19 @@ module.exports = {
     },
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
-        "project": "tsconfig.json",
-        "sourceType": "module",
+        "project": "tsconfig/tsconfig.lint.json",
+        "sourceType": "module"
     },
     "plugins": [
         "@typescript-eslint",
         "eslint-plugin-import",
         "eslint-plugin-jsdoc",
-        "prettier",
-        "deprecation"
+        "prettier"
     ],
     "rules": {
         "@typescript-eslint/no-explicit-any": "off",
         "@typescript-eslint/explicit-module-boundary-types": "off",
-        "@typescript-eslint/no-unused-vars": "warn",
-        "deprecation/deprecation": "warn",
-        "import/no-cycle": ["error", { "maxDepth": 15 }],
+        "import/no-cycle": "error",
         "import/no-unresolved": "off",
         "prettier/prettier": ["error"],
         "jsdoc/check-tag-names": ["error", { "definedTags": ["category"] }],
